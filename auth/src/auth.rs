c564b67b303847402b5dd2664f5679b8445346f7
Mees Molenaar <39046318+Mees-Molenaar@users.noreply.github.com>
2024-06-24 15:22:31 +0200

    chore: remove unused package and code

--- auth/src/auth.rs
+++ auth/src/auth.rs
@@ -1,34 +1,12 @@
 use std::fmt::Display;
-use std::fs;
 
 use axum::{
-    async_trait,
-    extract::FromRequestParts,
-    http::{request::Parts, StatusCode},
-    response::{IntoResponse, Json, Response}, RequestPartsExt,
+    http::StatusCode,
+    response::{IntoResponse, Json, Response},
 };
-use axum_extra::{
-    headers::{authorization::Bearer, Authorization},
-    TypedHeader,
-};
-use jsonwebtoken::{decode, DecodingKey, EncodingKey, Validation};
-use once_cell::sync::Lazy;
+
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-
-pub struct Keys {
-    pub encoding: EncodingKey,
-    pub decoding: DecodingKey,
-}
-
-impl Keys {
-    fn new(secret: &[u8]) -> Self {
-        Self {
-            encoding: EncodingKey::from_secret(secret),
-            decoding: DecodingKey::from_secret(secret),
-        }
-    }
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
@@ -64,30 +42,3 @@ impl IntoResponse for AuthError {
         (status, body).into_response()
     }
 }
-
-#[async_trait]
-impl<S> FromRequestParts<S> for Claims
-where
-    S: Send + Sync,
-{
-    type Rejection = AuthError;
-
-    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
-        // Extract the token from the authorization header
-        let TypedHeader(Authorization(bearer)) = parts
-            .extract::<TypedHeader<Authorization<Bearer>>>()
-            .await
-            .map_err(|_| AuthError::InvalidToken)?;
-        // Decode the user data
-        let token_data = decode::<Claims>(bearer.token(), &KEYS.decoding, &Validation::default())
-            .map_err(|_| AuthError::InvalidToken)?;
-
-        Ok(token_data.claims)
-    }
-}
-
-pub static KEYS: Lazy<Keys> = Lazy::new(|| {
-    let secret_file = std::env::var("JWT_SECRET_FILE").expect("JWT_SECRET_FILE must be set");
-    let secret = fs::read_to_string(secret_file).expect("Failed to read JWT secret file");
-    Keys::new(secret.as_bytes())
-});
