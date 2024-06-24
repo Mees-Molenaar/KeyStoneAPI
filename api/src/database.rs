7e8553827440832df3814b99f579a0a76f95716a
Mees Molenaar <39046318+Mees-Molenaar@users.noreply.github.com>
2024-06-24 15:22:07 +0200

    chore: use env variable for connection string in api instead of docker secret
    Want to deploy it on aws ecs and that does not support docker secrets

--- api/src/database.rs
+++ api/src/database.rs
@@ -3,10 +3,8 @@ use std::env;
 use sqlx::{postgres::PgPoolOptions, PgPool};
 
 pub async fn setup_database() -> Result<PgPool, sqlx::Error> {
-    let db_connect_str_file = env::var("DATABASE_CONNECTION_STRING_FILE")
-        .expect("DATABASE_CONNECTION_STRING_FILE must be set");
-    let db_connection_str = std::fs::read_to_string(db_connect_str_file)
-        .expect("DATABASE_CONNECTION_STRING_FILE must be a valid file path");
+    let db_connection_str = env::var("DATABASE_CONNECTION_STRING")
+        .expect("DATABASE_CONNECTION_STRING must be set");
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
