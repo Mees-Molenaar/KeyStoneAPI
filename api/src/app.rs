2f8ca182329290879e0e6ab7b66a20052d8796c9
Mees Molenaar <39046318+Mees-Molenaar@users.noreply.github.com>
2024-06-28 07:01:49 +0200

    docs: add openapi spec using the utoipa crate

--- api/src/app.rs
+++ api/src/app.rs
@@ -1,14 +1,45 @@
 use axum::{
     routing::{get, post},
-    Router,
+    Json, Router,
 };
 use sqlx::PgPool;
+use utoipa::{openapi::security::{HttpAuthScheme, HttpBuilder, SecurityScheme}, Modify, OpenApi};
+use utoipa_scalar::{Scalar, Servable as ScalarServable};
 
-use crate::routes::document::{create_document, get_document, get_documents};
+use super::routes::document::{create_document, get_document, get_documents, DocumentApi};
+
+#[derive(OpenApi)]
+#[openapi(
+    modifiers(&SecurityAddon),
+    nest(
+        (path ="/document", api=DocumentApi, tags=["Document"]),
+    )
+)]
+struct ApiDoc;
+
+struct SecurityAddon;
+
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        if let Some(components) = openapi.components.as_mut() {
+            components.add_security_scheme(
+                "Access Token",
+                SecurityScheme::Http(
+                    HttpBuilder::new().scheme(HttpAuthScheme::Bearer).bearer_format("JWT").build()
+                ))
+        }
+    }
+}
 
 pub async fn setup_app(pool: PgPool) -> Router {
     Router::new()
         .route("/document", post(create_document).get(get_documents))
         .route("/document/:id", get(get_document))
+        .route("/openapi", get(get_openapi_spec))
+        .merge(Scalar::with_url("/docs", ApiDoc::openapi()))
         .with_state(pool)
+}
+
+async fn get_openapi_spec() -> Json<utoipa::openapi::OpenApi> {
+    Json(ApiDoc::openapi())
 }
--- api/src/routes/document.rs
+++ api/src/routes/document.rs
@@ -5,15 +5,28 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
+use utoipa::{OpenApi, ToSchema};
 
 use crate::auth::Claims;
 
-#[derive(Deserialize, Debug, Serialize)]
+#[derive(OpenApi)]
+#[openapi(
+    paths(create_document, get_document, get_documents),
+    components(schemas(NewDocument, Document, )),
+    tags(
+        (
+            name = "Document",
+            description = "Document related operations"
+        )
+    ))]
+pub struct DocumentApi;
+
+#[derive(Deserialize, Debug, Serialize, ToSchema)]
 pub struct NewDocument {
     title: String,
     content: String,
 }
-#[derive(Deserialize, Serialize, Debug, sqlx::FromRow)]
+#[derive(Deserialize, Serialize, Debug, sqlx::FromRow, ToSchema)]
 pub struct Document {
     id: i32,
     user_id: String,
@@ -25,6 +38,15 @@ pub struct Document {
     last_synced_at: Option<chrono::NaiveDateTime>,
 }
 
+#[utoipa::path(
+    post,
+    path = "",
+    request_body = NewDocument,
+    responses(
+        (status = 201, description = "Document saved successfully", body = Document),
+        (status = 500, description = "Failed to save document")
+    )
+)]
 pub async fn create_document(
     State(pool): State<PgPool>,
     claims: Claims,
@@ -54,6 +76,16 @@ pub async fn create_document(
     Ok((StatusCode::CREATED, Json(result)))
 }
 
+#[utoipa::path(
+    get,
+    path = "",
+    responses(
+        (
+            status = 200, description = "List all documents", body = [Document])
+            , 
+            (status = 500, description = "Failed to get documents")
+    )
+)]
 pub async fn get_documents(
     State(pool): State<PgPool>,
     claims: Claims
@@ -76,6 +108,17 @@ pub async fn get_documents(
     Ok((StatusCode::OK, Json(documents)))
 }
 
+#[utoipa::path(
+    put,
+    path = "/{id}",
+    responses(
+        (status = 200, description = "Get a document by id", body = Document),
+        (status = 500, description = "Failed to get document")
+    ),
+    params(
+        ("id" = i32, Path, description = "Document database id")
+    )
+)]
 pub async fn get_document(
     State(pool): State<PgPool>,
     Path(id): Path<i32>,
